3e1d05233c1da88343d123dc14872b22bc488b15
Johannes Noebauer <johannes@noebauer.eu>
2025-06-25 09:11:14 +0200

    Add files via upload

---
+++ medianwert.py
@@ -0,0 +1,12 @@
+
+# %% Medianwert ermitteln
+import statistics
+
+# Beispielhafte Daten
+daten = [1, 3, 2, 4, 5]
+
+# Median berechnen
+median = statistics.median(daten)
+
+# Ergebnis ausgeben
+print(f"Der Median ist: {median}")
